5afee33ec2c02d39008bf5fd8872eeeac988cda1
Iris Osegueda <iriosegueda@gmail.com>
2016-11-29 03:04:56 -0600

    added initial commit of Account.java, has missing functions

---
+++ Account.java
@@ -0,0 +1,59 @@
+public class Account 
+{
+	private String owner;
+	private int accountNumber;
+	private double balance;
+	
+	public Account()
+	{
+		owner = "";
+		accountNumber = 0;
+		balance = 0.0;
+	}
+	public Account (String o, int n, double b)
+	{
+		setOwner(o);
+		setAccountNumber(n);
+		setBalance(b);
+	}
+	
+	public String getOwner()
+	{
+		return owner;
+	}
+	public int getAccountNumber()
+	{
+		return accountNumber;
+	}
+	public double getBalance()
+	{
+		return balance;
+	}
+	public void setOwner(String o)
+	{
+		owner = o;
+	}
+	public void setAccountNumber(int n)
+	{
+		accountNumber = n;
+	}
+	public void setBalance(double b)
+	{
+		balance = b;
+	}
+	public void withDraw(double amt)
+	{
+		
+	}
+	public void deposit(double amt)
+	{
+		
+	}
+	public String toString()
+	{
+		System.out.println("Owner: " + owner + "\n" +
+							"Account Number: " + accountNumber + "\n" +
+								"Account Balance: " + balance);
+	}
+	
+}
